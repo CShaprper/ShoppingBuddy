db426038b4cb935fec52f8d4510fb71b347b0745
Peter Sypek <petersypek@peters-imac.local>
2017-08-08 20:48:58 +0200

    test

--- ShoppingBuddy/DashboardController.swift
+++ ShoppingBuddy/DashboardController.swift
@@ -13,12 +13,7 @@ import CoreLocation
 class DashboardController: UIViewController, IFirebaseWebService{
     //MARK: - Outlets
     @IBOutlet var BackgroundView: UIImageView!
-    @IBOutlet var MenuContainer: UIView!
-    @IBOutlet var RoundMenu: UIImageView!
-    @IBOutlet var ButtonStack: UIStackView!
-    @IBOutlet var btn_Stores: UIButton!
-    @IBOutlet var btn_ShoppingList: UIButton!
-    @IBOutlet var MapView: MKMapView!
+     @IBOutlet var MapView: MKMapView!
     
     
     //MARK: - Member
@@ -80,29 +75,7 @@ class DashboardController: UIViewController, IFirebaseWebService{
     }
     func SegueToLoginController(sender: Notification) -> Void{
         performSegue(withIdentifier: String.SegueToLoginController_Identifier, sender: nil)
-    }
-    func MenuBarButtonItemPressed(sender: UIBarButtonItem) -> Void{
-        if isMenuOpened{
-            isMenuOpened = false
-            UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
-                self.RoundMenu.transform = CGAffineTransform(translationX: self.RoundMenu.frame.size.width, y: 0)
-            })
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
-                self.btn_Stores.transform = CGAffineTransform(translationX: self.MenuContainer.frame.width, y: 0)
-                self.btn_ShoppingList.transform = CGAffineTransform(translationX: self.MenuContainer.frame.width, y: 0)
-            })
-        } else {
-            isMenuOpened = true
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
-                self.RoundMenu.transform = .identity
-            })
-            UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .allowUserInteraction, animations: {
-                self.btn_Stores.transform = .identity
-                self.btn_ShoppingList.transform = .identity
-            })
-        }
-    }
-    
+    }    
     
     //MARK: - Helper Functions
     func ConfigureView() -> Void {
@@ -126,22 +99,7 @@ class DashboardController: UIViewController, IFirebaseWebService{
         logoutButton.setTitleTextAttributes([NSShadowAttributeName:shadow, NSStrokeWidthAttributeName:-1, NSStrokeColorAttributeName:UIColor.black, NSForegroundColorAttributeName:UIColor.ColorPaletteTintColor(), NSFontAttributeName:UIFont(name: "Courgette-Regular", size: 17)!], for: .normal)
         self.navigationItem.leftBarButtonItem = logoutButton
         
-        //MenuButton
-        let menuButton = UIBarButtonItem(image: #imageLiteral(resourceName: "icon-HamburgerMenu") , style: .plain, target: self, action: #selector(MenuBarButtonItemPressed))
-        navigationItem.rightBarButtonItem = menuButton
-        
         //Notification Listener
         NotificationCenter.default.addObserver(self, selector: #selector(SegueToLoginController), name: NSNotification.Name.SegueToLogInController, object: nil)
-        
-        //RoundMenu initial position
-        RoundMenu.transform = CGAffineTransform(translationX: RoundMenu.frame.size.width, y: 0)
-        
-        //Button Stores
-        btn_Stores.tintColor = UIColor.ColorPaletteSecondDarkest()
-        btn_Stores.transform = CGAffineTransform(translationX: MenuContainer.frame.width, y: 0)
-        
-        //Button ShoppingList
-        btn_ShoppingList.tintColor = UIColor.ColorPaletteSecondDarkest()
-        btn_ShoppingList.transform = CGAffineTransform(translationX: MenuContainer.frame.width, y: 0)
     }
 }
