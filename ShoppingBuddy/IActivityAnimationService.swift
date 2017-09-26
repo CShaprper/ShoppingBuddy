337627a7b1638e67565faa2e668f870e54748228
Peter Sypek <petersypek@peters-imac.local>
2017-09-26 19:01:07 +0200

    Magnifying glass added Needs some update in cancel sharing by shared user

---
+++ ShoppingBuddy/IActivityAnimationService.swift
@@ -0,0 +1,15 @@
+//
+//  IActivityAnimationService.swift
+//  ShoppingBuddy
+//
+//  Created by Peter Sypek on 18.08.17.
+//  Copyright © 2017 Peter Sypek. All rights reserved.
+//
+
+import Foundation
+
+@objc protocol IActivityAnimationService {
+    @objc optional var activityAnimationServiceDelegate:IActivityAnimationService? { get set }
+    @objc optional func ShowActivityIndicator()
+    @objc optional func HideActivityIndicator()
+}
