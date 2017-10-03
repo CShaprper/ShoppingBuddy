1630591442f5e84f43836ab2f6e800628a015d50
CShaprper <p.sypek@icloud.com>
2017-10-03 18:57:52 +0200

    Notes paper visual change

---
+++ ShoppingBuddy/SoundPlayer.swift
@@ -0,0 +1,29 @@
+//
+//  SounPlayer.swift
+//  ShoppingBuddy
+//
+//  Created by Peter Sypek on 02.10.17.
+//  Copyright © 2017 Peter Sypek. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundPlayer {
+    static var audioPlayer:AVAudioPlayer!
+    
+    static func PlaySound(filename:String, filetype:String) -> Void {
+        if let path = Bundle.main.path(forResource: filename, ofType: filetype){
+            let url = URL(fileURLWithPath: path)
+            audioPlayer = try? AVAudioPlayer(contentsOf: url, fileTypeHint: nil)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }
+        
+        /*let sound = UserDefaults.standard.bool(forKey: eUserDefaultKeys.SoundsOn.rawValue)
+        if sound {
+            audioPlayer.play()
+        }*/
+    }
+    
+}
